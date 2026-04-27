6766d656b4f2ef773ec903eed2cfe7ee7306f368
Archil Bolkvadze <a.bolkvadze@gmail.com>
2026-04-27 17:08:32 +0400

    dtb

--- space-time-grammar.js
+++ space-time-grammar.js
@@ -1,260 +1,131 @@
-const dot     = (o) => o[0]() 
-const S_t1    = (o) => o[2]('b', dot)
-const S_t22   = (o) => o[2]('a', dot)
-const S_t2    = (o) => o[1](S, S_t22)
-const S_s2    = (o) => o[1](dot, S_t2)
-const S       = (o) => o[1](S_s2, S_t1)
+const dot     = (d,t,b) => d() 
+const S_t1    = (d,t,b) => t('b', dot)
+const S_t22   = (d,t,b) => t('a', dot)
+const S_t2    = (d,t,b) => b(S, S_t22)
+const S_s2    = (d,t,b) => b(dot, S_t2)
+const S       = (d,t,b) => b(S_s2, S_t1)
 
-const tab     = (o) => o[2]('t',
-                (o) => o[2]('a',
-                (o) => o[2]('b', dot)))
+const tab     = (d,t,b) => t('t',
+                (d,t,b) => t('a',
+                (d,t,b) => t('b', dot)))
 
-const tritab13= (o) => o[1](tab, dot)
-const tritab12= (o) => o[1](tab, tritab13)
-const tritab11= (o) => o[1](tab, tritab12)
-const tritab  = (o) => o[1](dot, tritab11)
+const tritab13= (d,t,b) => b(tab, dot)
+const tritab12= (d,t,b) => b(tab, tritab13)
+const tritab11= (d,t,b) => b(tab, tritab12)
+const tritab  = (d,t,b) => b(dot, tritab11)
 
-const alfz              = (o) => o[2]("z",               dot)
-const alfy              = (o) => o[2]("y",               alfz)
-const alfx              = (o) => o[2]("x",               alfy)
-const alfw              = (o) => o[2]("w",               alfx)
-const alfv              = (o) => o[2]("v",               alfw)
-const alfu              = (o) => o[2]("u",               alfv)
-const alft              = (o) => o[2]("t",               alfu)
-const alfs              = (o) => o[2]("s",               alft)
-const alfr              = (o) => o[2]("r",               alfs)
-const alfq              = (o) => o[2]("q",               alfr)
-const alfp              = (o) => o[2]("p",               alfq)
-const alfo              = (o) => o[2]("o",               alfp)
-const alfn              = (o) => o[2]("n",               alfo)
-const alfm              = (o) => o[2]("m",               alfn)
-const alfl              = (o) => o[2]("l",               alfm)
-const alfk              = (o) => o[2]("k",               alfl)
-const alfj              = (o) => o[2]("j",               alfk)
-const alfi              = (o) => o[2]("i",               alfj)
-const alfh              = (o) => o[2]("h",               alfi)
-const alfg              = (o) => o[2]("g",               alfh)
-const alff              = (o) => o[2]("f",               alfg)
-const alfe              = (o) => o[2]("e",               alff)
-const alfd              = (o) => o[2]("d",               alfe)
-const alfc              = (o) => o[2]("c",               alfd)
-const alfb              = (o) => o[2]("b",               alfc)
-const alfa              = (o) => o[2]("a",               alfb)
-const name_t1           = (o) => o[1](alfa,              dot)
-const name_t2           = (o) => o[1](name,              name_t1)
-const name_s2           = (o) => o[1](dot,               name_t2)
-const name              = (o) => o[1](name_s2,           name_t1)
-const quantifier3       = (o) => o[2]("*",               dot);
-const quantifier2       = (o) => o[2]("+",               quantifier3);
-const quantifier1       = (o) => o[2]("?",               quantifier2);
-const quantifier        = (o) => o[2](" ",               quantifier1);
-const space_char2       = (o) => o[2]("\n",              dot)
-const space_char1       = (o) => o[2]("\t",              space_char2)
-const space_char        = (o) => o[2](" ",               space_char1)
-const sp_t1             = (o) => o[1](space_char,        dot)
-const sp_t2             = (o) => o[1](sp,                sp_t1)
-const sp_s2             = (o) => o[1](dot,               sp_t2)
-const sp                = (o) => o[1](sp_s2,             sp_t1)
-const primary_member_t1 = (o) => o[1](name,              dot)
-const primary_member_t22= (o) => o[2]('\'',              dot)
-const primary_member_t21= (o) => o[1](alfa,              primary_member_t22)
-const primary_member_t2 = (o) => o[2]('\'',              primary_member_t21)
-const primary_member_t34= (o) => o[2](')',               dot)
-const primary_member_t33= (o) => o[1](sp,                primary_member_t34)
-const primary_member_t32= (o) => o[1](productions,       primary_member_t33)
-const primary_member_t31= (o) => o[1](sp,                primary_member_t32)
-const primary_member_t3 = (o) => o[2]('(',               primary_member_t31)
-const primary_member_s3 = (o) => o[1](dot,               primary_member_t3) 
-const primary_member_s2 = (o) => o[1](primary_member_s3, primary_member_t2) 
-const primary_member    = (o) => o[1](primary_member_s2, primary_member_t1)
-const member_t11        = (o) => o[1](quantifier,        dot) 
-const member_t1         = (o) => o[1](primary_member,    member_t11) 
-const member            = (o) => o[1](dot,               member_t1)
-const production_t1     = (o) => o[1](member,            dot)
-const production_t21    = (o) => o[1](sp,                production_t1)
-const production_t2     = (o) => o[1](production,        production_t21)
-const production_s2     = (o) => o[1](dot,               production_t2)
-const production        = (o) => o[1](production_s2,     production_t1)
-const productions_t1    = (o) => o[1](production,        dot)
-const productions_t23   = (o) => o[1](sp,                productions_t1)
-const productions_t22   = (o) => o[2]('|',               productions_t23)
-const productions_t21   = (o) => o[1](sp,                productions_t22)
-const productions_t2    = (o) => o[1](productions,       productions_t21)
-const productions_s2    = (o) => o[1](dot,               productions_t2)
-const productions       = (o) => o[1](productions_s2,    productions_t1)
-const declaration_t16   = (o) => o[2](';',               dot)
-const declaration_t15   = (o) => o[1](sp,                declaration_t16)
-const declaration_t14   = (o) => o[1](productions,       declaration_t15)
-const declaration_t13   = (o) => o[1](sp,                declaration_t14)
-const declaration_t12   = (o) => o[2](':',               declaration_t13)
-const declaration_t11   = (o) => o[1](sp,                declaration_t12)
-const declaration_t1    = (o) => o[1](name,              declaration_t11)
-const declaration       = (o) => o[1](dot,               declaration_t1)
-const declarations_t1   = (o) => o[1](declaration,       dot)
-const declarations_t21  = (o) => o[1](sp,                declarations_t1)
-const declarations_t2   = (o) => o[1](declarations,      declarations_t21)
-const declarations_s2   = (o) => o[1](dot,               declarations_t2)
-const declarations      = (o) => o[1](declarations_s2,   declarations_t1)
+const alfz = (d,t,b) => t("z",               dot)
+const alfy = (d,t,b) => t("y",               alfz)
+const alfx = (d,t,b) => t("x",               alfy)
+const alfw = (d,t,b) => t("w",               alfx)
+const alfv = (d,t,b) => t("v",               alfw)
+const alfu = (d,t,b) => t("u",               alfv)
+const alft = (d,t,b) => t("t",               alfu)
+const alfs = (d,t,b) => t("s",               alft)
+const alfr = (d,t,b) => t("r",               alfs)
+const alfq = (d,t,b) => t("q",               alfr)
+const alfp = (d,t,b) => t("p",               alfq)
+const alfo = (d,t,b) => t("o",               alfp)
+const alfn = (d,t,b) => t("n",               alfo)
+const alfm = (d,t,b) => t("m",               alfn)
+const alfl = (d,t,b) => t("l",               alfm)
+const alfk = (d,t,b) => t("k",               alfl)
+const alfj = (d,t,b) => t("j",               alfk)
+const alfi = (d,t,b) => t("i",               alfj)
+const alfh = (d,t,b) => t("h",               alfi)
+const alfg = (d,t,b) => t("g",               alfh)
+const alff = (d,t,b) => t("f",               alfg)
+const alfe = (d,t,b) => t("e",               alff)
+const alfd = (d,t,b) => t("d",               alfe)
+const alfc = (d,t,b) => t("c",               alfd)
+const alfb = (d,t,b) => t("b",               alfc)
+const alfa = (d,t,b) => t("a",               alfb)
 
-const write = (...args) => process.stdout.write(...args)
-const symbols = [tab, tritab, S, declarations]
-let        cs = 0
-let      next = dot
-const stm = (f,...args) => setTimeout(f, 200,...args)
-const time = [
-  (    ) => { write('.\n'), next(space) },
-  (s, t) => { if(symbols.indexOf(s) === -1) symbols.push(s);
-              write(' ' + symbols.indexOf(s)), stm(t, time); },
-  (x, t) => { write(' ' + JSON.stringify(x)), stm(t, time); },
-]
-const space = [
-  (    ) => { write(';\n');
-              if(++cs < symbols.length)
-                 console.log(cs), symbols[cs](space); },
-  (s, t) => { write('  '), next = s, stm(t, time); },
-  (x, s) => { write('  ' + JSON.stringify(x) + '.\n'), stm(s, space); },
-]
-console.log(cs), symbols[cs](space)
+const name_t1 = (d,t,b) => b(alfa,              dot)
+const name_t2 = (d,t,b) => b(name,              name_t1)
+const name_s2 = (d,t,b) => b(dot,               name_t2)
+const name    = (d,t,b) => b(name_s2,           name_t1)
 
-//console.log('// ' + Object.keys(symbols).join(', '))
+const quantifier3 = (d,t,b) => t("*",               dot);
+const quantifier2 = (d,t,b) => t("+",               quantifier3);
+const quantifier1 = (d,t,b) => t("?",               quantifier2);
+const quantifier  = (d,t,b) => t(" ",               quantifier1);
 
-//const gram = `
-//alfa
-//  'a'
-//  'b'
-//  'c'
-//  'd'
-//  'e'
-//  'f'
-//  'g'
-//  'h'
-//  'i'
-//  'j'
-//  'k'
-//  'l'
-//  'm'
-//  'n'
-//  'o'
-//  'p'
-//  'q'
-//  'r'
-//  's'
-//  't'
-//  'u'
-//  'v'
-//  'w'
-//  'x'
-//  'y'
-//  'z'
-//name
-//  alfa
-//  name alfa
-//quantifier
-//  '?'
-//  '+'
-//  '*'
-//  ' '
-//space_char
-//  ' '
-//  '\t'
-//  '\n'
-//sp
-//  space_char
-//  sp space_char
-//primary_member
-//  name
-//  '\'' alfa '\''
-//  '(' sp productions sp ')'
-//member
-//  primary_member quantifier
-//production
-//  member
-//  production sp member
-//productions
-//  production
-//  productions sp '|' sp production
-//declaration
-//  name sp ':' sp productions sp ';'
-//declarations
-//  declaration
-//  declarations sp declaration
-//`.split('\n').map(l => 1 < l.length && l[0] === ' ' && l[1] === ' ' ? '\t' +l.trim(): l.trim()).filter(Boolean).join('\n')
-//// expr   : add ;
-//// add    : mul (('+'|'-') mul)* ;
-//// mul    : atom (('*'|'/') atom)* ;
-//// atom   : INT | '(' expr ')' ;
-//// n1 m11 m12 | m21 m22 ;
-//// n2 m11 m12 | m21 m22 ;
-////
-//
-//const print_bnf = [
-//  () => write('.\n'),
-//  (lhs) => write(lhs + ' →'),
-//  (rhs) => write(' ' + rhs),
-//  () => {},
-//]
-////bnf(tritab)(print_bnf)
-////bnf(S)(print_bnf)
-//const map = {}
-//let cur = '';
-//bnf(declarations)([
-//  () => {},
-//  (lhs) => {
-//    if (map[lhs]) {
-//      map[lhs].push([])
-//    } else {
-//      map[lhs] = [[]];
-//    }
-//    cur = lhs;
-//  },
-//  (rhs) => map[cur][map[cur].length-1].push(rhs),
-//  () => {},
-//])
-//console.log(
-//  Object.keys(map).map(n => 
-//    n +'\n' + map[n].map(p => '  ' + p.join(' ')).join('\n')
-//  ).join('\n')
-//)
-//
-//
-//// The stabilizer: wraps recursion in a map to prevent infinite loops,
-//// identifying cycles in the "soul."
-//const SpaceTime = (S) => {
-//  const Space = (S, d) =>
-//    d.has(S) ? dot : d.set(S, {
-//      [S.name]: o => S([
-//        o[0],
-//        (s, t) => o[1](Space(s, d), Time(t, d)),
-//        (x, s) => o[2](x, Space(s, d)),
-//      ])
-//    }[S.name]).get(S)
-//  const Time = (S, d) =>
-//    d.has(S) ? dot : d.set(S, {
-//      [S.name]: o => S([
-//        o[0],
-//        (s, t) => o[1](Space(s, new Map()), Time(t, d)),
-//        (x, t) => o[2](x, Time(t, d)),
-//      ])
-//    }[S.name]).get(S)
-//  return Space(S, new Map())
-//}
-//
-//// unified space time structure into
-//// Backus-Naur Form for human observation.
-//function bnf(S, d = new Map()) {
-//  return o => {
-//    if(d.has(S)) return o[3]()
-//    d.set(S)
-//    const space = [
-//      (    ) => o[3](),
-//      (s, t) => (o[1](S.name), t(time), s(space)),
-//      (x, s) => (o[1](S.name), o[2](JSON.stringify(x)), o[0](), s(space)),
-//    ]
-//    const time  = [
-//      (    ) => o[0](),
-//      (s, t) => (o[2](s.name), t(time), bnf(s, d)(o)),
-//      (x, t) => (o[2](JSON.stringify(x)), t(time)),
-//    ]
-//    S(space)
-//  }
-//}
+const space_char2 = (d,t,b) => t("\n",              dot)
+const space_char1 = (d,t,b) => t("\t",              space_char2)
+const space_char  = (d,t,b) => t(" ",               space_char1)
+
+const sp_t1 = (d,t,b) => b(space_char,        dot)
+const sp_t2 = (d,t,b) => b(sp,                sp_t1)
+const sp_s2 = (d,t,b) => b(dot,               sp_t2)
+const sp    = (d,t,b) => b(sp_s2,             sp_t1)
+
+const primary_member_t1 = (d,t,b) => b(name,              dot)
+const primary_member_t22= (d,t,b) => t('\'',              dot)
+const primary_member_t21= (d,t,b) => b(alfa,              primary_member_t22)
+const primary_member_t2 = (d,t,b) => t('\'',              primary_member_t21)
+const primary_member_t34= (d,t,b) => t(')',               dot)
+const primary_member_t33= (d,t,b) => b(sp,                primary_member_t34)
+const primary_member_t32= (d,t,b) => b(productions,       primary_member_t33)
+const primary_member_t31= (d,t,b) => b(sp,                primary_member_t32)
+const primary_member_t3 = (d,t,b) => t('(',               primary_member_t31)
+const primary_member_s3 = (d,t,b) => b(dot,               primary_member_t3) 
+const primary_member_s2 = (d,t,b) => b(primary_member_s3, primary_member_t2) 
+const primary_member    = (d,t,b) => b(primary_member_s2, primary_member_t1)
+
+const member_t11 = (d,t,b) => b(quantifier,        dot) 
+const member_t1  = (d,t,b) => b(primary_member,    member_t11) 
+const member     = (d,t,b) => b(dot,               member_t1)
+
+const production_t1  = (d,t,b) => b(member,            dot)
+const production_t21 = (d,t,b) => b(sp,                production_t1)
+const production_t2  = (d,t,b) => b(production,        production_t21)
+const production_s2  = (d,t,b) => b(dot,               production_t2)
+const production     = (d,t,b) => b(production_s2,     production_t1)
+
+const productions_t1  = (d,t,b) => b(production,        dot)
+const productions_t23 = (d,t,b) => b(sp,                productions_t1)
+const productions_t22 = (d,t,b) => t('|',               productions_t23)
+const productions_t21 = (d,t,b) => b(sp,                productions_t22)
+const productions_t2  = (d,t,b) => b(productions,       productions_t21)
+const productions_s2  = (d,t,b) => b(dot,               productions_t2)
+const productions     = (d,t,b) => b(productions_s2,    productions_t1)
+
+const declaration_t14 = (d,t,b) => t(';',               dot)
+const declaration_t13 = (d,t,b) => b(sp,                declaration_t14)
+const declaration_t12 = (d,t,b) => b(productions,       declaration_t13)
+const declaration_t11 = (d,t,b) => b(sp,                declaration_t12)
+const declaration_t1  = (d,t,b) => b(name,              declaration_t11)
+const declaration     = (d,t,b) => b(dot,               declaration_t1)
+
+const declarations_t1  = (d,t,b) => b(declaration,       dot)
+const declarations_t21 = (d,t,b) => b(sp,                declarations_t1)
+const declarations_t2  = (d,t,b) => b(declarations,      declarations_t21)
+const declarations_s2  = (d,t,b) => b(dot,               declarations_t2)
+const declarations     = (d,t,b) => b(declarations_s2,   declarations_t1)
+
+bnf_compiler(S, tritab, declarations);
+
+function bnf_compiler(...symbols) {
+  const write = (...args) => process.stdout.write(...args)
+  let cs = 0
+  let next = dot
+
+  fetch()
+
+  function time_dot       (   ) { write('\n'), space(next) }
+  function time_terminal  (x,t) { write(JSON.stringify(x) + ' '), time(t) }
+  function time_branch    (s,t) { if(symbols.indexOf(s) === -1) symbols.push(s)
+                                  write(s.name + ' '), time(t) }
+  function space_dot      (   ) { fetch() }
+  function space_terminal (x,s) { write('  ' + JSON.stringify(x) + '\n'), space(s) }
+  function space_branch   (s,t) { write('  '), next = s, time(t) }
+
+  function time (s) { s(time_dot, time_terminal, time_branch) }
+  function space(s) { s(space_dot, space_terminal, space_branch) }
+
+  function fetch() {
+    if(cs < symbols.length) 
+      next = symbols[cs++], write(next.name + '\n'), space(next)
+  }
+}
