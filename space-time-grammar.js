bae216d5575eb5af8300585517ea469564f44e91
Archil Bolkvadze <a.bolkvadze@gmail.com>
2026-04-27 19:36:09 +0400

    pausable

--- space-time-grammar.js
+++ space-time-grammar.js
@@ -114,31 +114,32 @@ function bnf_compiler(...symbols) {
   fetch()
 
   function time_dot         (   ) { next_space(next) }
-  function time_terminal    (x,t) { write(JSON.stringify(x)), next_time(t) }
+  function time_termin      (x,t) { write(JSON.stringify(x)), stm(next_time, t) }
   function time_branch      (s,t) { if(symbols.indexOf(s) === -1)
                                       symbols.push(s)
-                                    write(s.name), next_time(t) }
-  function next_time_termin (x,t) { write(' '), time_terminal(x, t) }
+                                    write(s.name), stm(next_time, t) }
+  function next_time_termin (x,t) { write(' '), time_termin(x, t) }
   function next_time_branch (s,t) { write(' '), time_branch(s, t) }
 
-  function space_dot        (   ) { write('.\n'),             fetch() }
-  function space_termin     (x,s) { write(JSON.stringify(x)), next_space(s)}
+  function space_dot        (   ) { write('.\n'),             stm(fetch) }
+  function space_termin     (x,s) { write(JSON.stringify(x)), stm(next_space, s)}
   function space_branch     (s,t) { next = s,                 init_time(t)}
 
   function init_space_termin(x,s) { write('  '), space_termin(x, s) }
   function init_space_branch(s,t) { write('  '), space_branch(s, t) }
-  function next_space_termin(x,s) { write(', '), space_termin(x, s) }
-  function next_space_branch(s,t) { write(', '), space_branch(s, t) }
+  function next_space_termin(x,s) { write(', '), stm(space_termin, x, s) }
+  function next_space_branch(s,t) { write(', '), stm(space_branch, s, t) }
 
-  function init_time (s) { s(time_dot, time_terminal, time_branch) }
-  function next_time (s) { s(time_dot, next_time_termin, next_time_branch) }
+  function init_time (s) { s(time_dot,  time_termin,       time_branch) }
+  function next_time (s) { s(time_dot,  next_time_termin,  next_time_branch) }
   function init_space(s) { s(space_dot, init_space_termin, init_space_branch) }
   function next_space(s) { s(space_dot, next_space_termin, next_space_branch) }
 
   function fetch() {
     if(cs < symbols.length) 
-      next = symbols[cs++], write(next.name + '\n'), init_space(next)
+      next = symbols[cs++], write(next.name + '\n'), stm(init_space, next)
   }
+  function stm(f,...args) { setTimeout(f, 200, ...args) }
 }
 
 // expr add.
