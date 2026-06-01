db4099ab9f35dc3a0c5bb4f9b93d27ae3f67ac33
Archil Bolkvadze <a.bolkvadze@gmail.com>
2026-06-01 12:23:26 +0400

    clean open diagram

--- open_diagram.js
+++ open_diagram.js
@@ -1,21 +1,19 @@
-import zlib from 'zlib'
-import child_process from 'child_process'
-import os from 'os'
+import zlib from 'node:zlib'
+import child_process from 'node:child_process'
+import os from 'node:os'
 
 export default function open_diagram(...args) {
   const state = {
     code: mermaid(...args),
     grid: true,
-    mermaid: '{\n  "theme": "dark"\n}',
-    //panZoom: true,
-    //rough: false,
-    //updateDiagram: true,
-    //renderCount: 5,
-    //pan: { x: 507.3228154144735, y: 52.80805721952268 },
-    //zoom: 0.875,
-    //editorMode: 'code'
+    mermaid: '{ "theme": "dark" }',
   }
-  openBrowser('https://mermaid.live/view#pako:' + zlib.deflateSync(Buffer.from(JSON.stringify(state), 'utf-8')).toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, ''));
+  const graph = zlib.deflateSync(Buffer.from(JSON.stringify(state), 'utf-8'))
+    .toString('base64')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=+$/, '')
+  openBrowser('https://mermaid.live/view#pako:' + graph);
   function openBrowser(url) {
     const { exec } = child_process;
     const platform = os.platform();
@@ -23,41 +21,38 @@ export default function open_diagram(...args) {
       platform === 'win32' ? `start "" "${url}"` :
       platform === 'darwin' ? `open "${url}"` :
       `xdg-open "${url}"`;
-
     exec(cmd);
   }
-  function mermaid(...symbols) {
-    const attrs = []
-    const edges = []
-    const iof = s => {
-      let i = symbols.indexOf(s)
-      if (i === -1) i = symbols.length, symbols.push(s)
-      return i
-    } 
-    for(let i = 0; i < symbols.length; i++) {
-      const s = symbols[i] 
-      const n = s.name
-      s((      q) =>  attrs[i] = [0, n, q], 
-        (x, u, q) => (attrs[i] = [1, n, q, x], edges.push([i, iof(u), 1])),
-        (s, t, q) => (attrs[i] = [2, n, q],    edges.push([i, iof(s), 0]),
-                                               edges.push([i, iof(t), 1])))
-    }
-    return (`---
+}
+function mermaid(...symbols) {
+  const attrs = []
+  const edges = []
+  const iof = s => {
+    let i = symbols.indexOf(s)
+    if (i === -1) i = symbols.length, symbols.push(s)
+    return i
+  } 
+  for(let i = 0; i < symbols.length; i++) {
+    const s = symbols[i] 
+    const n = s.name
+    s((      q) =>  attrs[i] = [0, n, q], 
+      (x, u, q) => (attrs[i] = [1, n, q, x], edges.push([i, iof(u), 1])),
+      (s, t, q) => (attrs[i] = [2, n, q],    edges.push([i, iof(s), 0]),
+        edges.push([i, iof(t), 1])))
+  }
+  return (
+`---
 config:
   layout: elk
 ---
-flowchart TB
-` +
-      attrs.map(([op,n,q,x],i) => i + (
-         op === 0 ? '@{ shape: framed-circle }'
-        :op === 1 ? '['+JSON.stringify(x.replace(/"/g,"&quot;"))+']'
-        :           '(['+(n||i)+'])')
-        ).join('\n')
-        + '\n' +
-      attrs.map(([op,n,q,x],i) => `style ${i} color:${q?'Black':'White'},stroke:${q?'Black':'White'},fill:${q}`).join('\n')
-        + '\n' +
-        edges.map(([a,b,c]) => a+(c ?' -':' .')+'-> '+b).join('\n')
-      )
-  }
+flowchart TB`
+    + '\n' +  attrs.map(([op,n,q,x],i) => i + (
+        op === 0 ? '@{ shape: framed-circle }'
+      : op === 1 ? '['+JSON.stringify(x.replace(new RegExp('"', 'g'), "&quot;"))+']'
+      :            '(['+(n||i)+'])'
+    )).join('\n')
+    + '\n' +  attrs.map(([op,n,q,x],i) => 'style '+i+' color:Black,stroke:Black,fill:'+(q || "Gray")).join('\n')
+    + '\n' +  edges.map(([a,b,c]) => a+(c ?' -':' .')+'-> '+b).join('\n')
+  )
 }
 
