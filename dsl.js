bd3607371e08fc9924210b950a307faa097e4eac
Archil Bolkvadze <a.bolkvadze@gmail.com>
2026-06-02 13:23:20 +0400

    explicit dsl

--- dsl.js
+++ dsl.js
@@ -1,13 +1,13 @@
-import pmap from './pmap.js'
-const oDSL = pmap(S => {
+const ds = Symbol()
+const oDSL = S => {
+  if (S[ds]) return S[ds]
   const times = [] 
   S((...spaces) => times.push(spaces.reverse().reduce(
-    (t, s) => typeof s === "string"
-  ? (_,B,__) => B(s, t)
-  : (_,__,T) => T(oDSL(s), t),
-    (D,_,__) => D()
+    (t, s) => typeof s === "string" ? (_,B,__) => B(s, t)
+                                    : (_,__,T) => T(oDSL(s), t),
+                                      (D,_,__) => D()
   )))
-  return times.reverse().reduce((s, t) => (_,__,T) => T(s, t), (D,_,__) => D())
-})
+  return S[ds] = times.reverse().reduce((s, t) => (_,__,T) => T(s, t), (D,_,__) => D())
+}
 export default oDSL
 
